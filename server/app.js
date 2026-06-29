eff8d0432e5356571027f58fab85e359f010c94a
Shivanshi Yadav <yadavshivanshi636@gmail.com>
2026-06-30 01:58:55 +0530

    Configure production API and CORS

--- server/app.js
+++ server/app.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+    "https://civic-watch-ai-two.vercel.app",],
     credentials: true,
   })
 );
